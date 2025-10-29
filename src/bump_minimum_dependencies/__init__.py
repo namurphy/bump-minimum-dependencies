bcef31688690f714c312e1d0bfe6120d5d7b7f3f
Nick Murphy <namurphy@cfa.harvard.edu>
2025-10-29 16:41:23 -0400

    Initialize click command

--- src/bump_minimum_dependencies/__init__.py
+++ src/bump_minimum_dependencies/__init__.py
@@ -1,2 +1,25 @@
-def main() -> None:
-    print("Hello from bump-minimum-dependencies!")
+import click
+
+
+@click.command()
+@click.option("pyproject_file", default="pyproject.toml", help="Path to pyproject.toml")
+@click.option("months", default=24, help="Drop releases from this many months ago.")
+@click.option(
+    "buffer",
+    default=6,
+    help="Ensure that there is at least one release this many months old.",
+)
+@click.option(
+    "python_months", default=36, help="Drop Python releases from this many months ago."
+)
+@click.option(
+    "date", default="today", help="Date to use instead of today (YYYY-MM-DD)."
+)
+def main(
+    pyproject_file: str, months: str, buffer: str, python_months: str, date: str
+) -> None:
+    click.echo(pyproject_file)
+    click.echo(months)
+    click.echo(buffer)
+    click.echo(python_months)
+    click.echo(date)
