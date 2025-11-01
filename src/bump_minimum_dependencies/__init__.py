4418494a86d04f25d19807bd92efc9f19d0071e2
Nick Murphy <namurphy@cfa.harvard.edu>
2025-10-31 20:15:32 -0400

    Remove unused options for CLI

--- src/bump_minimum_dependencies/__init__.py
+++ src/bump_minimum_dependencies/__init__.py
@@ -13,27 +13,13 @@ from . import bump
     default=6,
     help="Ensure that there is at least one release this many months old.",
 )
-@click.option(
-    "--python_months",
-    default=36,
-    help="Drop Python releases from this many months ago.",
-)
-@click.option(
-    "--date",
-    default="today",
-    help="Date to use instead of today (YYYY-MM-DD).",
-)
 def main(
     pyproject_file: str,
     months: int,
     buffer: int,
-    python_months: int,
-    date: str,
 ) -> None:
     bump.bump_minimum_dependencies(
         pyproject_file=pyproject_file,
         months=months,
         buffer=buffer,
-        python_months=python_months,
-        date=date,
     )
