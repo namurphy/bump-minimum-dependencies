ef9c76d4fe55d52a8a4aae4fa6398806db878b6c
Nick Murphy <namurphy@cfa.harvard.edu>
2025-11-13 22:46:45 -0500

    Major updates to test_bump.py

--- tests/test_bump.py
+++ tests/test_bump.py
@@ -1,80 +1,54 @@
 import shutil
 
+
 from bump_minimum_dependencies import bump
-from astropy.time import Time
+from datetime import date
 import pytest
 
 from pathlib import Path
 
 baseurl = "https://raw.githubusercontent.com/namurphy/bump-minimum-dependencies/refs/heads/main/tests/data/"
 
-start_of_2026 = Time("2026-01-01 00:00:01.000000")
-
-# def plasmapy():
-#     name = "plasmapy"
-#     instance = bump.Package(
-#         name=name,
-#         url=f"{baseurl}{name}.json",
-#     )
-#     instance.now = start_of_2026
-#     return instance
-#
-#
-# def numpy():
-#     name = "numpy"
-#     instance = bump.Package(
-#         name=name,
-#         url=f"{baseurl}{name}.json",
-#     )
-#     instance.now = start_of_2026
-#     return instance
-
-
 @pytest.mark.parametrize(
-    "name, months, buffer, expected",
+    "name, drop_months, cooldown_months, expected",
     [
-        ("plasmapy", 24, 0, "/2024.2"),
+        ("plasmapy", 24, 0, "2024.2"),
         ("plasmapy", 4, 0, "2025.10"),
         ("plasmapy", 4, 3, "2025.8"),
         ("plasmapy", 48, 0, "0.8.1"),
         ("plasmapy", 1, 0, "2025.10"),
         ("numpy", 24, 0, "2"),
         ("numpy", 24, 23, "1.26"),
+        ("astropy", 24, 12, "6.1"),
+        ("astropy", 24, 23, "6"),
+        ("pyproject-fmt", 100, 100, "0.1"),
     ],
 )
-def test_plasmapy(name, months, buffer, expected):
-    package = bump.Package(name=name, url=f"{baseurl}{name}.json")
-    package.now = start_of_2026
-    release = package.last_supported_release(months=months, buffer=buffer)
+def test_bump(name, drop_months, cooldown_months, expected, freezer):
+    freezer.move_to("2026-01-01")
+    package = bump.Package(name=name)
+    release = package.oldest_supported_minor_release(
+        drop_months=drop_months, cooldown_months=cooldown_months
+    )
     assert str(release) == expected
 
 
-#
-# @pytest.mark.parametrize(
-#     "months_buffer_expected",
-#     [
-#         (24, 0, "2024.2"),
-#         (4, 0, "2025.10"),
-#         (4, 3, "2025.8"),
-#         (48, 0, "0.8.1"),
-#         (1, 0, "2025.10"),
-#     ]
-# )
-# def test_numpy(plasmapy, months_buffer_expected):
-#     months, buffer, expected = months_buffer_expected
-#     release = plasmapy.last_supported_release(months=months, buffer=buffer)
-#     assert str(release) == expected
-
-
-@pytest.mark.parametrize("months, buffer", [(4, 5), (4, 4), (-1, 0), (0, -1)])
-def test_months_exceeds_buffer(months, buffer):
-    package = bump.Package(name="plasmapy", url=f"{baseurl}plasmapy.json")
+@pytest.mark.parametrize(
+    "drop_months, cooldown_months", [(4, 5), (-1, 0), (0, -1)]
+)
+def test_drop_months_ge_cooldown_months(drop_months, cooldown_months):
+    package = bump.Package(name="plasmapy")
     with pytest.raises(ValueError):
-        package.last_supported_release(months=months, buffer=buffer)
+        package.oldest_supported_minor_release(
+            drop_months=drop_months, cooldown_months=cooldown_months
+        )
 
 
-@pytest.mark.xfail
-def test_pyproject(tmp_path, monkeypatch):
+#@pytest.mark.xfail
+def test_pyproject(tmp_path, monkeypatch, freezer):
+
+    freezer.move_to("2026-01-01")
+
     data_dir = Path(__file__).parent / "data"
     original_pyproject = data_dir / "pyproject.toml"
     expected_pyproject = data_dir / "pyproject.expected.toml"
@@ -83,10 +57,7 @@ def test_pyproject(tmp_path, monkeypatch):
     shutil.copy(original_pyproject, pyproject)
     monkeypatch.chdir(tmp_path)
 
-    bump.bump_minimum_dependencies(months=24, buffer=6)
-
-    # if filecmp.cmp(pyproject, expected_pyproject, shallow=False):
-    #     return
+    bump.bump_minimum_dependencies(drop_months=24, cooldown_months=21)
 
     with open(pyproject) as f1:
         actual = f1.readlines()
