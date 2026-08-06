40b9ea64abcdb6535d94f5fa6989f406e5c015e1
Nick Murphy <namurphy@cfa.harvard.edu>
2026-08-06 18:11:18 -0400

    Add lots more options

--- src/bump_minimum_dependencies/__init__.py
+++ src/bump_minimum_dependencies/__init__.py
@@ -7,23 +7,56 @@ from . import bump
 
 @click.command()
 @click.option(
-    "--pyproject_file", default="pyproject.toml", help="Path to pyproject.toml"
+    "--pyproject_file",
+    default="pyproject.toml",
+    help="Path to pyproject.toml",
 )
 @click.option(
-    "--drop_months", default=24, help="Drop releases from this many months ago."
+    "--drop-months",
+    default=24,
+    help="Drop minor releases from this many months ago.",
 )
 @click.option(
-    "--cooldown_months",
+    "--cooldown-months",
     default=12,
     help="Ensure that there is at least one release this many months old.",
+)
+@click.option(
+    "--all-extras",
+    default=False,
+    is_flag=True,
+    help="Update all optional dependencies.",
+)
+@click.option(
+    "--all-groups", default=False, is_flag=True, help="Update all dependency groups."
+)
+@click.option(
+    "--extra",
+    default=[],
+    help="Name of an optional dependencies category. May be provided more than once.",
+    multiple=True,
+)
+@click.option(
+    "--group",
+    default=[],
+    help="Name of a dependency group to update. May be provided more than once.",
+    multiple=True,
 )
 def main(
     pyproject_file: str,
     drop_months: int,
     cooldown_months: int,
+    all_extras: bool,
+    all_groups: bool,
+    extra: tuple[str, ...] | list[str],
+    group: tuple[str, ...] | list[str],
 ) -> None:
     bump.bump_minimum_dependencies(
         pyproject_file=pyproject_file,
         drop_months=drop_months,
         cooldown_months=cooldown_months,
+        all_extras=all_extras,
+        all_groups=all_groups,
+        extra=extra,
+        group=group,
     )
