b772cf37e0fb8b0250951d866cc66ba7eb9ecc3b
Nick Murphy <namurphy@cfa.harvard.edu>
2026-08-06 14:23:43 -0400

    Update sessions and docstrings in noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,6 @@
 # /// script
-# python_version = "==3.14"
-# dependencies = ["nox[uv]"]
+# python_version = ">=3.13"
+# dependencies = ["nox", "nox-uv"]
 # ///
 
 import nox
@@ -11,31 +11,38 @@ nox.options.default_venv_backend = "uv"
 
 _HERE = pathlib.Path(__file__).parent
 
-MAXPYTHON = "3.14"
+supported_python_versions: tuple[str, ...] = ("3.13", "3.14")
+maxpython: str = sorted(supported_python_versions)[-1]
 
 
-@nox_uv.session(python=MAXPYTHON, uv_groups=["lint"])
+@nox_uv.session(python=maxpython, uv_groups=["dev"])
 def lint(session: nox.Session) -> None:
+    """Run prek on all files."""
     session.run("prek", "run", "--all-files", "--quiet")
 
 
-@nox_uv.session(python=MAXPYTHON, uv_groups=["test"])
+@nox_uv.session(python=supported_python_versions, uv_groups=["dev"])
 def test(session: nox.Session) -> None:
+    """Run tests."""
     session.run("pytest", "tests", "--tb=short")
 
 
-@nox_uv.session(python=MAXPYTHON, uv_groups="[ty]")
+@nox_uv.session(python=maxpython, uv_groups=["dev"])
 def ty(session: nox.Session) -> None:
-    session.run("ty", "check", ".")
+    """Perform static type checking with ty. """
+    args = session.posargs or ["--fix"]
+    session.run("ty", "check", ".", *args)
 
 
-@nox.session(python=MAXPYTHON)
+@nox.session(python=supported_python_versions)
 def build(session: nox.Session) -> None:
+    """Build the package."""
     session.run("uv", "build")
 
 
-@nox.session(python=MAXPYTHON)
+@nox.session(python=supported_python_versions)
 def run(session: nox.Session) -> None:
+    """Run the package."""
     session.install(".")
     session.run("bump-minimum-dependencies")
 
